6b22fcebbaaf5f89076f666186b84384a3bc0bb1
gujiacheng <gujiacheng@ctticsh.cn>
2023-08-08 10:44:04 +0800

    🎉init

---
+++ apps/server/src/db/index.ts
@@ -0,0 +1,11 @@
+import Mongoose from "mongoose";
+
+const dbUrl = "mongodb+srv://admin:11182203gjc@cluster0.g5rfvk1.mongodb.net/";
+
+Mongoose.connect(dbUrl)
+	.then(() => {
+		console.log("MongoDB connected!");
+	})
+	.catch((err) => console.log(err));
+
+export default Mongoose;
---
+++ apps/server/src/server.ts
@@ -0,0 +1,20 @@
+import { json, urlencoded } from "body-parser";
+import express from "express";
+import morgan from "morgan";
+
+export const createServer: () => express.Express = () => {
+	const app = express();
+	app
+		.disable("x-powered-by")
+		.use(morgan("dev"))
+		.use(urlencoded({ extended: true }))
+		.use(json())
+		.get("/message/:name", (req, res) => {
+			return res.json({ message: `hello ${req.params.name}` });
+		})
+		.get("/healthz", (req, res) => {
+			return res.json({ message: `now: ${new Date()}` });
+		});
+
+	return app;
+};
