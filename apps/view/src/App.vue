13c2fb7cf9817fdc9a72a99d804ff353aab87df2
gujiacheng <gujiacheng@ctticsh.cn>
2023-08-11 12:12:48 +0800

    测试

--- apps/view/src/App.vue
+++ apps/view/src/App.vue
@@ -3,7 +3,9 @@ import { onMounted, ref } from "vue";
 
 const respnseStr = ref();
 
-const testApi = fetch("/api/healthz").then(function (response) {
+const testApi = fetch("https://cashapi.amon1118.top/book/list").then(function (
+	response
+) {
 	//response.status表示响应的http状态码
 	if (response.status === 200) {
 		//json是返回的response提供的一个方法,会把返回的json字符串反序列化成对象,也被包装成一个Promise了
