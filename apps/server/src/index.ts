01f1197e622a5aec2e8f91bd09eb1fb1c90de77d
gujiacheng <gujiacheng@ctticsh.cn>
2023-09-05 15:35:09 +0800

    🔧config: test

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,6 +1,6 @@
 import { createServer } from "./server";
 
-const port = process.env.PORT || 5001;
+const port = process.env.PORT || 5000;
 const server = createServer();
 server.listen(port, () => {
 	console.log(`api running on ${port}`);
